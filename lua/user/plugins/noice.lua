1df1374ca3615579862e37fe63cf902995a25e5b
maxoja <tawan.tpptnk@gmail.com>
2022-12-23 22:03:35 +0000

    not noice (disable cmdPalette/notify/message)

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -23,4 +23,16 @@ noice.setup({
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
     },
+
+    cmdline = {
+        enabled = false,
+    },
+
+    notify = {
+        enabled = false,
+    },
+
+    messages = {
+        enabled = false,
+    },
 })
