4f817c87fc41c101b2899b09cc67329974ed1ad5
maxoja <tawan.tpptnk@gmail.com>
2022-12-23 20:33:15 +0000

    something with nvim-transparent

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -202,14 +202,14 @@ return packer.startup(function(use)
     use { "windwp/nvim-autopairs", config = function() require("user.plugins.autopairs") end }
     use { "folke/zen-mode.nvim", config = function() require("zen-mode").setup({
             window = {
-                width = 150,
+                width = 100,
                 options = {
                     wrap = true,
                 }
             }
         })
     end }
-    use { "xiyaowong/nvim-transparent", config = function() require("transparent").setup({ enable = not vim.g.neovide }) end }
+    use { "xiyaowong/nvim-transparent", config = function() require("transparent").setup({ enable = false}) end }
     use { "jinh0/eyeliner.nvim", config = function() require("user.plugins.eyeliner") end }
     use {
         "nvim-neo-tree/neo-tree.nvim",
